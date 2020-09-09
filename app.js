488d57326a0493434ab40c6b9c68325d9fc8eafc
Mo.Rehan <morehanp@gmail.com>
2020-09-09 13:45:56 +0600

    application almost completed

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const fs = require("fs");
+#!/usr/bin/env node
 
-fs.readdir(process.cwd(), (err, files) => {
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+
+//# Method 2
+// const  lastat  = util.promisify(fs.lstat);
+
+//# Method No: 3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, files) => {
   if (err) throw new Error(err);
-  console.log(files);
+  const filesStatPromises = files.map((file) => lstat(file));
+  const allData = await Promise.all(filesStatPromises);
+
+  for (data of allData) {
+    const index = allData.indexOf(data);
+
+    data.isFile() === true
+      ? console.log(files[index])
+      : chalk.green.bgWhite.bold(files[index]);
+  }
 });
 
+//# Method 1
 
+// const stat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stat) => {
+//       if (err) reject(err);
+//       resolve(stat);
+//     });
+//   });
+// };
